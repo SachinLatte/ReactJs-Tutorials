121e1f6f075af89c3886c595b446173a2a299aea
SachinLatte <sachin@digitallatte.in>
2025-01-30 18:33:18 +0530

    calculator app UI done\

--- Fragments/learning-fragments/src/App.jsx
+++ Fragments/learning-fragments/src/App.jsx
@@ -3,13 +3,22 @@ import "./App.css";
 import FoodItems from "./componets/FoodItems";
 import FoodHeading from "./componets/FoodHeading";
 import ErrorMessage from "./componets/ErrorMessage";
+import Container from "./componets/Container";
 function App() {
-  let foodItems = ["Sabji", "Chicken", "Roti", "Salad", "Milk"];
+  let foodItems = ["Sabji", "Chicken", "Roti", "Salad", "Milk", "Butter"];
   return (
     <>
-      <FoodHeading />
-      <ErrorMessage data={foodItems}></ErrorMessage>
-      <FoodItems data={foodItems}></FoodItems>
+      <Container>
+        <FoodHeading />
+        <ErrorMessage data={foodItems}></ErrorMessage>
+        <FoodItems data={foodItems}></FoodItems>
+      </Container>
+      <Container>
+        <p>
+          Above is the list of healthy foods that are good for your health and
+          well being.
+        </p>
+      </Container>
     </>
   );
 }
--- Projects/calculator/calculator/src/components/Button.jsx
+++ Projects/calculator/calculator/src/components/Button.jsx
@@ -1,11 +1,29 @@
 import styles from "./Button.module.css";
 const Button = () => {
+  const buttonNames = [
+    "C",
+    "1",
+    "2",
+    "+",
+    "3",
+    "4",
+    "-",
+    "5",
+    "6",
+    "*",
+    "7",
+    "8",
+    "/",
+    "=",
+    "9",
+    "0",
+    ".",
+  ];
   return (
     <>
-      <button className={styles.button}>C</button>
-      <button className={styles.button}>1</button>
-      <button className={styles.button}>2</button>
-      <button className={styles.button}>+</button>
+      {buttonNames.map((elem) => (
+        <button className={styles.button}>{elem}</button>
+      ))}
     </>
   );
 };
